fbcceaab0b95d1f927a025edbb32bd4e40be29a0
yufengyuan77 <131631595+yufengyuan77@users.noreply.github.com>
2026-06-03 21:26:16 +0800

    Add files via upload
    项目进度：
    主要变更study.tsx
    首页            ✅
    导航            ✅
    课程中心        ✅
    课程分类切换    ✅
    Mock数据        ✅
    React状态管理   ✅

---
+++ src/mock/courses.ts
@@ -0,0 +1,38 @@
+export const courses = [
+  {
+    id: 1,
+    title: '行测基础课程',
+    lessons: 30,
+    category: '行测',
+  },
+  {
+    id: 2,
+    title: '言语理解专项',
+    lessons: 20,
+    category: '行测',
+  },
+  {
+    id: 3,
+    title: '判断推理专题',
+    lessons: 25,
+    category: '行测',
+  },
+  {
+    id: 4,
+    title: '申论基础课程',
+    lessons: 18,
+    category: '申论',
+  },
+  {
+    id: 5,
+    title: '申论写作提升',
+    lessons: 15,
+    category: '申论',
+  },
+  {
+    id: 6,
+    title: '结构化面试',
+    lessons: 12,
+    category: '面试',
+  },
+]
--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -1,5 +1,66 @@
+import { useState } from 'react'
+
+import { courses } from '../mock/courses'
+
 function Study() {
-  return <h1>学 - 课程中心</h1>
+
+  const [category, setCategory] = useState('行测')
+
+console.log(category)
+
+const filteredCourses = courses.filter(
+  (course) => course.category === category
+)
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8">
+        课程中心
+      </h1>
+
+      {/* 课程分类 */}
+      <div className="flex gap-4 mb-8">
+        <button
+        onClick={() => setCategory('行测')}
+        className="px-6 py-2 bg-blue-500 text-white rounded-lg">
+          行测
+        </button>
+
+        <button 
+        onClick={() => setCategory('申论')}
+        className="px-6 py-2 bg-white rounded-lg shadow">
+          申论
+        </button>
+
+        <button 
+        onClick={() => setCategory('面试')}
+        className="px-6 py-2 bg-white rounded-lg shadow">
+          面试
+        </button>
+      </div>
+
+      {/* 课程列表 */}
+      <div className="grid grid-cols-3 gap-6">
+        {filteredCourses.map((course) => (
+          <div
+            key={course.title}
+            className="bg-white rounded-xl shadow p-6 hover:shadow-lg transition"
+          >
+            <div className="text-4xl text-blue-500 mb-4">
+            </div>
+
+            <h2 className="text-xl font-bold mb-2">
+              {course.title}
+            </h2>
+
+            <p className="text-gray-500">
+              共 {course.lessons} 课时
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Study
